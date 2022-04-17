f0ebcb68fa2d15b22ad89000064168e5de1b0f85
Xuan-Hieu <hieunxhe140911@fpt.edu.vn>
2022-04-17 19:14:44 +0700

    commit

---
+++ OnlineLearn/src/java/Entity/course.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author khait
+ */
+public class course {
+    
+    private int id;
+    private String name;
+    private String thumbnail;
+    private int categoryID;
+    private int feature;
+    private int ownerID;
+    private int status;
+    private String description;
+
+    public course() {
+    }
+
+    public course(int id, String name, String thumbnail, int categoryID, int feature, int ownerID, int status, String description) {
+        this.id = id;
+        this.name = name;
+        this.thumbnail = thumbnail;
+        this.categoryID = categoryID;
+        this.feature = feature;
+        this.ownerID = ownerID;
+        this.status = status;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public int getFeature() {
+        return feature;
+    }
+
+    public void setFeature(int feature) {
+        this.feature = feature;
+    }
+
+    public int getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(int ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "course{" + "id=" + id + ", name=" + name + ", thumbnail=" + thumbnail + ", categoryID=" + categoryID + ", feature=" + feature + ", ownerID=" + ownerID + ", status=" + status + ", description=" + description + '}';
+    }
+
+}
